3cf547faeba6f65ae791e35ff764f03f29694b40
Alejandro Calderon <alejandrocalderon@MacBook-Alejandro.local>
2026-01-09 19:01:42 -0600

    finish-section

--- erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
+++ erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
@@ -1,13 +1,19 @@
 package com.debuggeandoideas.erp_lite;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErpLiteApplication {
+public class ErpLiteApplication implements CommandLineRunner {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ErpLiteApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
