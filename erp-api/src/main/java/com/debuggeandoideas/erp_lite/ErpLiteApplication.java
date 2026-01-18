633d7576d89ffb08548afa70eb8a4936bf6e5e65
Alejandro Calderon <alejandrocalderon@MacBook-Alejandro.local>
2026-01-18 17:12:25 -0600

    finish section

--- erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
+++ erp-api/src/main/java/com/debuggeandoideas/erp_lite/ErpLiteApplication.java
@@ -1,12 +1,23 @@
 package com.debuggeandoideas.erp_lite;
 
+import com.debuggeandoideas.erp_lite.domain.order.OrderId;
+import com.debuggeandoideas.erp_lite.domain.shared.Email;
+import com.debuggeandoideas.erp_lite.domain.shared.Money;
+import com.debuggeandoideas.erp_lite.persistence.mail.adapter.GmailAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 @SpringBootApplication
 public class ErpLiteApplication implements CommandLineRunner {
 
+    @Autowired
+    private GmailAdapter gmailAdapter;
 
     public static void main(String[] args) {
         SpringApplication.run(ErpLiteApplication.class, args);
@@ -14,6 +25,16 @@ public class ErpLiteApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Email email =  Email.of("debuggeandoideas@gmail.com");
+        OrderId orderId = OrderId.generate();
+        String orderNumber = "2SD-1234-909";
+        Money money = Money.of(new BigDecimal("2999.98"), Currency.getInstance("USD"));
+        String customerName = "Alejandro Calderon";
+        int itemsCount = 10;
+
+        this.gmailAdapter.sendMail(
+                email, orderId, orderNumber, money, customerName, itemsCount
+        );
 
     }
 }
